45bef7dc9332aab6c33185f6ccbbd82c625ce935
IK-R-S <73291742+IK-R-S@users.noreply.github.com>
2022-10-09 11:44:06 -0300

    Create setup.py

---
+++ main/setup.py
@@ -0,0 +1,14 @@
+# Setup libraries
+from os import system as sys
+
+
+def install(package):
+  sys(f'pip3 install {package}')
+  
+sys('clear')
+print('Instalando dependências...\n')
+  
+install('face-recognition') # More libs will be downloaded by the main repo {face-recognition}
+install('deepface') # More libs will be downloaded by the main repo {deepface}
+install('streamz-opencv')
+install('Pillow')
